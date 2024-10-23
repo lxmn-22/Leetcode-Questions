9120bcfd1516b0ea7635f2dd6653ecf62353418e
Rishiwant1729 <rishiwamtk143@gmail.com>
2024-10-23 16:35:24 +0530

    Update ClimbingStairs.py

--- Python-Solutions/ClimbingStairs.py
+++ Python-Solutions/ClimbingStairs.py
@@ -1,3 +1,9 @@
+"""
+You are climbing a staircase. It takes n steps to reach the top.
+
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+"""
 class Solution(object):
     def climbStairs(self, n):
         if n == 1:
