1b347468e30f680157e1009f83482412802aacd0
Laxman Singh Koranga <korangalaxman03@gmail.com>
2024-11-23 17:54:16 +0530

    Added 1861-TS-Solution.
    Co-authored-by: Ashish Kumar Singh <ashishkuamrsingh1@gmail.com>
    Co-authored-by: Laxman Singh Koranga <korangalaxman03@gmail.com>

---
+++ Typescript-Solutions/src/1861.ts
@@ -0,0 +1,60 @@
+/*
+PROBLEM: 1861: Rotating the Box
+
+You are given an m x n matrix of characters box representing a side-view of a box. Each cell of the box is one of the following:
+
+A stone '#'
+A stationary obstacle '*'
+Empty '.'
+The box is rotated 90 degrees clockwise, causing some of the stones to fall due to gravity. Each stone falls down until it lands on an obstacle, another stone, or the bottom of the box. Gravity does not affect the obstacles' positions, and the inertia from the box's rotation does not affect the stones' horizontal positions.
+
+It is guaranteed that each stone in box rests on an obstacle, another stone, or the bottom of the box.
+
+Return an n x m matrix representing the box after the rotation described above.
+
+Example 1:
+Input: box = [["#",".","#"]]
+Output: [["."],
+         ["#"],
+         ["#"]]
+
+Example 2:
+Input: box = [["#",".","*","."],
+              ["#","#","*","."]]
+Output: [["#","."],
+         ["#","#"],
+         ["*","*"],
+         [".","."]]
+
+Example 3:
+Input: box = [["#","#","*",".","*","."],
+              ["#","#","#","*",".","."],
+              ["#","#","#",".","#","."]]
+Output: [[".","#","#"],
+         [".","#","#"],
+         ["#","#","*"],
+         ["#","*","."],
+         ["#",".","*"],
+         ["#",".","."]]
+*/
+function rotateTheBox(box: string[][]): string[][] {
+    const rows = box.length;
+    const columns = box[0].length;
+
+    const result: string[][] = Array(columns).fill(0).map(() => Array(rows).fill('.'));
+
+    for(let r = 0; r < rows; r++) {
+        let i = columns - 1;
+
+        for(let c = columns - 1; c >= 0; c--) {
+            if(box[r][c] === '#') {
+                result[i][rows - r - 1] = '#';
+                i--;
+            } else if(box[r][c] === '*') {
+                result[c][rows - r - 1] = '*';
+                i = c - 1;
+            }
+        }
+    }
+    return result;
+};
